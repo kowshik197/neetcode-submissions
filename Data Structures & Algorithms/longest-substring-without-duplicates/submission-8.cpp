e604f70b97eeea4aa46ec6248cb369990b492fb4
Kowshik Sivakumar <kowshik19072006@gmail.com>
2026-07-27 22:54:18 +0530

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   public:
+    int lengthOfLongestSubstring(string s) {
+        int l=0, r = 0;
+        int curr_best = 0;
+        unordered_set<char> st;
+        while (r < s.size()) {
+            if (st.find(s[r]) == st.end()) {
+                st.insert(s[r]);
+                curr_best = max(curr_best, r - l + 1);
+                r++;
+            }
+
+            else {
+                while (st.find(s[r]) != st.end()) {
+                    st.erase(s[l]);
+                    l++;
+                }
+            }
+        }
+        return curr_best;
+    }
+};
