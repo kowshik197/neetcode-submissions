69f01b3d528e8cda05ef4cb40938e1b570ed52e4
Kowshik Sivakumar <kowshik19072006@gmail.com>
2026-07-28 18:43:47 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,41 @@
+class TimeMap {
+public:
+
+    unordered_map<string, vector<pair<int,string>>> mp;
+
+    TimeMap() {
+
+    }
+
+    void set(string key, string value, int timestamp) {
+
+        mp[key].push_back({timestamp, value});
+    }
+
+    string get(string key, int timestamp) {
+
+        if (mp.find(key) == mp.end())
+            return "";
+
+        vector<pair<int,string>> &arr = mp[key];
+
+        int l = 0;
+        int r = arr.size() - 1;
+
+        if (arr[l].first > timestamp) return "";
+
+        if (arr[r].first <= timestamp) return arr[r].second;
+
+        while (r - l > 1) {
+
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid].first <= timestamp)
+                l = mid;
+            else
+                r = mid;
+        }
+
+        return arr[l].second;
+    }
+};
