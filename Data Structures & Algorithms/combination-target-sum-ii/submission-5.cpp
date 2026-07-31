9cb091cf6b3b732441de6efb4e7aaef2697e2167
Kowshik Sivakumar <kowshik19072006@gmail.com>
2026-07-31 19:50:00 +0530

    Add: combination-target-sum-ii - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-5.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    vector<vector<int>> result;
+    void backtrack(int start,vector<int>& candidates,int target,vector<int>& path) {
+
+        // Base Case
+        if (target == 0) {
+            result.push_back(path);
+            return;
+        }
+
+        if (target < 0)
+            return;
+
+        for (int i = start; i < candidates.size(); i++) {
+
+            if (i > start && candidates[i] == candidates[i - 1])   // Here first occurence is valid... so we make i>idx
+                continue; 
+            // Choose
+            path.push_back(candidates[i]);
+
+            // Explore
+            backtrack(i+1, candidates, target - candidates[i], path);
+
+            // Undo
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> path;
+        sort(candidates.begin(), candidates.end());
+        backtrack(0, candidates, target, path);
+
+        return result;
+    
+    }
+};
