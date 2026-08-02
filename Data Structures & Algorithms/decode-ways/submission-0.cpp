c5a1dd4de78ea0f3240f4e9949dc5e16be06b6af
Kowshik Sivakumar <kowshik19072006@gmail.com>
2026-08-02 18:58:32 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+string s="";
+int n;
+vector<int> memo;
+int f(int i){
+
+    if(i>=s.size()){
+            return 1;
+    }
+    if(s[i]=='0'){
+            return 0;
+    }
+
+    if(memo[i]!=-1){
+            return memo[i];
+    }
+
+    int ways =0;
+
+    ways+= f(i+1);
+
+    if(i+1<n && ((s[i]=='1') || (s[i]=='2' && s[i+1]<='6'))){
+        ways+=f(i+2);
+    }
+
+        return memo[i]=ways;
+
+}
+
+int numDecodings(string s) {
+    n=s.size();
+    this->s=s;
+    memo.assign(n,-1);
+   
+    return  f(0);
+       }
+};
