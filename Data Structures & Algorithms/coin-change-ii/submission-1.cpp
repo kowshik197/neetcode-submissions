4b36a2550253c5b3b75dc3c56b3c8fe63058c59c
Kowshik Sivakumar <kowshik19072006@gmail.com>
2026-08-10 15:47:04 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+
+        unordered_map<int,int> dp;
+
+        dp[0] = 1;
+
+        for(int coin : coins) {
+
+            for(int a = coin; a <= amount; a++) {
+
+                dp[a] += dp[a - coin];
+            }
+        }
+
+        return dp[amount];
+    }
+};
