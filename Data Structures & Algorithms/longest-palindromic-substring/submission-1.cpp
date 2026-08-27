0dac080baecca7d986664fa8510d11ed8e739475
Kowshik Sivakumar <kowshik19072006@gmail.com>
2026-08-27 11:28:09 +0530

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    int start = 0;
+    int maxLen = 1;
+
+    void expand(string& s, int left, int right) {
+
+        while(left >= 0 &&
+              right < s.size() &&
+              s[left] == s[right]) {
+
+            int len = right - left + 1;
+
+            if(len > maxLen) {
+                maxLen = len;
+                start = left;
+            }
+
+            left--;
+            right++;
+        }
+    }
+
+    string longestPalindrome(string s) {
+
+        int n = s.size();
+
+        if(n == 0) {
+            return "";
+        }
+
+        for(int i = 0; i < n; i++) {
+ expand(s, i, i + 1);
+            // odd length
+            expand(s, i, i);
+
+            // even length
+           
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
