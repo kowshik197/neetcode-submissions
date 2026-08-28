2aac8211446797fe6e8fe98c1ba2f072807648ee
Kowshik Sivakumar <kowshik19072006@gmail.com>
2026-08-28 14:24:39 +0530

    Add: cheapest-flight-path - submission-7

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-7.cpp
@@ -0,0 +1,22 @@
+//Bellman-Ford
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+
+        for (int i = 0; i <= k; i++) {
+            vector<int> temp = dist;
+            for (auto& flight : flights) {
+                int u = flight[0], v = flight[1], price = flight[2];
+                if(dist[u]==INT_MAX) continue;
+                if (dist[u] + price < temp[v]) {
+                    temp[v] = dist[u] + price;
+                }
+            }
+            dist = temp;
+        }
+
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
+    }
+};
