86df2ec0288f304c841b684414ce5dd21e37815c
Kowshik Sivakumar <kowshik19072006@gmail.com>
2026-09-06 17:35:33 +0530

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+// #include <algorithm>
+class Solution {
+public:
+    int reverse(int x) {
+        bool neg=false;
+        if(x<0){
+            neg=true;
+            x*=-1;
+        }
+        string val= to_string(x);
+
+       std::reverse(val.begin(),val.end());
+
+        long long ans=stoll(val);
+
+        if(neg){
+            ans*=-1;
+        }
+
+        if(ans>INT_MAX || ans<INT_MIN){
+            return 0;
+        }
+
+        return ans;
+
+    }
+};
