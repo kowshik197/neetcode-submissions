2440eb65362386c615c4dd2c93ac857ac93911c4
Kowshik Sivakumar <kowshik19072006@gmail.com>
2026-09-10 23:39:01 +0530

    Add: minimum-interval-including-query - submission-1

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-1.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<int> minInterval(vector<vector<int>>& intervals,
+                            vector<int>& queries) {
+
+        // Sort intervals by starting point
+        sort(intervals.begin(), intervals.end());
+
+        // Store {query, original index}
+        vector<pair<int,int>> qs;
+
+        for (int i = 0; i < queries.size(); i++) {
+            qs.push_back({queries[i], i});
+        }
+
+        // Sort queries
+        sort(qs.begin(), qs.end());
+
+        // {size, end}
+        priority_queue<
+            pair<int,int>,
+            vector<pair<int,int>>,
+            greater<pair<int,int>>
+        > pq;
+
+        vector<int> ans(queries.size(), -1);
+
+        int i = 0;
+
+        for (auto [q, index] : qs) {
+
+            // Add all intervals whose start <= q
+            while (i < intervals.size() &&
+                   intervals[i][0] <= q) {
+
+                int l = intervals[i][0];
+                int r = intervals[i][1];
+
+                int size = r - l + 1;
+
+                pq.push({size, r});// Key here, we are storing the end part which gets removed if query less than the end
+
+                i++;
+            }
+
+            // Remove intervals that cannot contain q
+            while (!pq.empty() &&
+                   pq.top().second < q) {
+
+                pq.pop();
+            }
+
+            // Smallest valid interval
+            if (!pq.empty()) {
+                ans[index] = pq.top().first;
+            }
+        }
+
+        return ans;
+    }
+};
